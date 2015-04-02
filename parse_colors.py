5c4bc59d4edc9c8a356646599079575bcec74d80
Peter Sobot <psobot@gmail.com>
2015-04-02 01:58:57 -0400

    basic

--- parse_colors.py
+++ parse_colors.py
@@ -3,10 +3,25 @@ import csv
 import json
 
 
+def all_languages_and_their_words():
+    for row in csv.DictReader(open(sys.argv[1], 'r')):
+        rowcopy = dict(row)
+        try:
+            del rowcopy['Language']
+            del rowcopy['#']
+            for k, v in dict(rowcopy).iteritems():
+                if v == "":
+                    del rowcopy[k]
+        except KeyError:
+            pass
+        yield (row.get('Language', None), rowcopy)
+
+
 def convert_to_json():
-    with open(sys.argv[1], 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        return [row for row in reader]
+    return dict(
+        (language, words)
+        for (language, words) in all_languages_and_their_words()
+    )
 
 
 print json.dumps(convert_to_json(), indent=2)
